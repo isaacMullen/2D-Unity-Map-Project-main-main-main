f2c22b128feb1082ef7d970f76348f460ca6a2ef
OTWhite04 <owen.t.white2005@gmail.com>
2024-11-15 20:15:36 -0400

    Player Movement Attempt

--- Assets/Scripts/Player Controls.cs
+++ Assets/Scripts/Player Controls.cs
@@ -4,6 +4,76 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class PlayerControls : MonoBehaviour
 {
-  
+    public Tilemap tilemap;
+    public Vector3Int currentTile;
+    public float moveSpeed = 1.0f;
+
+    private Vector3Int newTile;
+    private Vector3 target;
+   
+    void Start()
+    {
+        currentTile = tilemap.WorldToCell(transform.position);
+        target = position.transform;
+    }
+
+    void Update()
+    {
+        HandleInput();
+        MovePlayer();
+    }
+
+    //Method for moving the player on the map.
+    void HandleInput()
+    {
+        //Keycodes for the four different WASD directions.
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            newTile = currentTile + new Vector3Int(0, 1, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            newTile = currentTile + new Vector3Int(0, -1, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            newTile = currentTile + new Vector3Int(1, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            newTile = currentTile + new Vector3Int(-1, 0, 0);
+        }
+   
+        
+        
+    }
+
+    void MovePlayer()
+    {
+        transform.position = Vector3MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        
+        if(transform.position == target)
+        {
+            currentTile - newTile;
+        }
+    
+    }
+    //Bool that returns if a tile can be walked on or not with a true or false.
+    bool IsTileWalkable(Vector3Int tilePosition)
+    {
+        TileBase tile = tilemap.GetTile(tilePosition);
+
+        if(tile != null)
+        {
+
+            string tileName = tile.name;
+           //If statement for tiles that I don't want the player to walk on.
+            if(tileName == "Wall" || tileName == "Chest" || tileName == "Door")
+            {
+                return false;
+            }
+        }
+    }
+
 
 }
