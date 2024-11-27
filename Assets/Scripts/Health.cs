896f8ee0acd9ea50f9eb2a8b45e48afd13ec95b6
OTWhite04 <owen.t.white2005@gmail.com>
2024-11-27 15:09:38 -0400

    Commenting Code

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,20 +5,35 @@ using TMPro;
 
 public class Health : MonoBehaviour
 {
+    //Public textmeshpro for displaying the health, health status and lives.
     public TextMeshProUGUI textmeshpro;
+    //Public variables for the health and lives, which is declared on the variable.
     public int health;
+    public int lives = 2;
+    //Public string for writing the health status of the player.
     public string healthStatus;
    
-    
+    //Method that prints the health, health status and number of lives the player has into the game.
     public string ShowHUD()
     {
         healthStatus = HealthStatus(health);
-        return $"Health: {health}";
+        return $"Health: {health}" + $"Lives: {lives}" + $"Health Status: {healthStatus}";
     }
 
+    //Method for player taking damage.
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+        //If statement for player health reaching zero, makes sure health doesn't go into the negatives
+        //and resets their stats with the revive method.
+        if(health <= 0)
+        {
+            health = 0;
+            Revive();
+        }
+    }
 
-
-
+    //Method for the health status that lets the player know how badly they are injured depending on the range of their health.
     public string HealthStatus(int hp)
     {
         if (hp <= 100 && hp > 90)
@@ -42,8 +57,31 @@ public class Health : MonoBehaviour
             return "Imminent Danger";
         }
 
+        //Returns out of range incase the health goes outside the 0 - 100 range for whatever reason.
         return "Out of Range";
 
     }
+
+    //Method for reviving the player after they lose a life, resets health to 100.
+    public void Revive()
+    {
+        lives = lives - 1;
+        health = 100;
+
+        //Resets game if player has no more lives.
+        if(lives <= 0)
+        {
+            ResetGame();
+        }
+    }
+
+    //Resets the health and lives values after the player dies.
+    public void ResetGame()
+    {
+        health = 100;
+        lives = 2;
+    }
+
+
 
 }
