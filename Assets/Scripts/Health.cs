095e4fc8364dccc419f19e7cd62599fae98c033f
OTWhite04 <owen.t.white2005@gmail.com>
2024-11-27 13:18:04 -0400

    Working On Deliverable

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public string healthStatus;
+   
+    
+    public string ShowHUD()
+    {
+        healthStatus = HealthStatus(health);
+        return $"Health: {health}";
+    }
+
+
+
+
+    public string HealthStatus(int hp)
+    {
+        if (hp <= 100 && hp > 90)
+        {
+            return "Perfect Health";
+        }
+        else if (hp <= 90 && hp > 75)
+        {
+            return "Healthy";
+        }
+        else if (hp <= 75 && hp > 50)
+        {
+            return "Hurt";
+        }
+        else if (hp <= 50 && hp > 10)
+        {
+            return "Badly Hurt";
+        }
+        else if (hp <= 10 && hp >= 1)
+        {
+            return "Imminent Danger";
+        }
+
+        return "Out of Range";
+
+    }
+
+}
---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("2DRPG");
+    }
+   
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls Screen");
+    }
+
+
+
+
+
+}
